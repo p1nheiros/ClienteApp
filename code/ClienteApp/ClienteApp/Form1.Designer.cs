4975fc47a661ee401bc24f67d1fc3aaf7d83e96c
p1nheiros <pinheiros.dev@gmail.com>
2024-11-14 02:35:04 -0300

    subindo codigo finalizado do crud de clientes com validações.

---
+++ code/ClienteApp/ClienteApp/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClienteApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxLimite = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(516, 306);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(573, 28);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(65, 20);
+            this.textBoxID.TabIndex = 1;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(573, 83);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(180, 20);
+            this.textBoxNome.TabIndex = 2;
+            // 
+            // textBoxLimite
+            // 
+            this.textBoxLimite.Location = new System.Drawing.Point(573, 143);
+            this.textBoxLimite.Name = "textBoxLimite";
+            this.textBoxLimite.Size = new System.Drawing.Size(180, 20);
+            this.textBoxLimite.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(570, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(570, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nome";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(570, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Limite de Crédito";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(573, 182);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(180, 36);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "Salvar";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(573, 228);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(180, 36);
+            this.buttonUpdate.TabIndex = 8;
+            this.buttonUpdate.Text = "Atualizar";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.White;
+            this.buttonDelete.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonDelete.Location = new System.Drawing.Point(573, 273);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(180, 36);
+            this.buttonDelete.TabIndex = 9;
+            this.buttonDelete.Text = "Deletar";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 330);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxLimite);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Carregar os dados na grid ao abrir o formulário
+                LoadDataGrid();
+
+                // Configuração inicial dos botões
+                buttonSave.Enabled = true;
+                buttonUpdate.Enabled = false;
+                buttonDelete.Enabled = false;
+
+                // Limpar os campos de entrada
+                ClearFields();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar o formulário: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0) // Verifica se o clique foi em uma célula válida
+            {
+                string cellValue = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value?.ToString() ?? "Vazio";
+                MessageBox.Show($"Célula clicada: {cellValue}", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.TextBox textBoxLimite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonUpdate;
+        private Button buttonDelete;
+    }
+}
+
---
+++ code/ClienteApp/ClienteApp/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Npgsql;
+using System.Text.RegularExpressions;
+
+namespace ClienteApp
+{
+    public partial class Form1 : Form
+    {
+        private readonly string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=clientes_db";
+
+        private string currentNome;
+        private decimal currentLimite;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Configuração inicial dos botões
+            buttonSave.Enabled = false;
+            buttonUpdate.Enabled = false;
+            buttonDelete.Enabled = false;
+
+            // Conecta os eventos aos botões e à grid
+            buttonSave.Click += ButtonSave_Click;
+            buttonUpdate.Click += ButtonUpdate_Click;
+            buttonDelete.Click += ButtonDelete_Click;
+            dataGridView1.CellClick += DataGridView1_CellClick;
+
+            // Adiciona validações aos campos de entrada
+            textBoxID.TextChanged += ValidateInputs;
+            textBoxNome.TextChanged += ValidateInputs;
+            textBoxLimite.TextChanged += ValidateInputs;
+
+            // Adiciona restrições de entrada nos campos
+            textBoxID.KeyPress += (s, e) => RestrictToNumbers(e);
+            textBoxNome.KeyPress += (s, e) => RestrictToLetters(e);
+            textBoxLimite.KeyPress += (s, e) => RestrictToDecimal(textBoxLimite, e);
+
+            // Carrega os dados ao iniciar
+            LoadDataGrid();
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Deseja realmente salvar este registro?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirmResult == DialogResult.Yes)
+            {
+                SaveRecord();
+            }
+        }
+
+        private void SaveRecord()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxNome.Text) || string.IsNullOrWhiteSpace(textBoxLimite.Text) || string.IsNullOrWhiteSpace(textBoxID.Text))
+            {
+                MessageBox.Show("Preencha todos os campos antes de salvar!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!decimal.TryParse(textBoxLimite.Text, out _))
+            {
+                MessageBox.Show("Insira um valor numérico válido para o limite de crédito!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (var transaction = connection.BeginTransaction())
+                    {
+                        var query = "INSERT INTO clientes (idcliente, nome_cliente, limite_credito) VALUES (@id, @nome, @limite)";
+                        using (var command = new NpgsqlCommand(query, connection, transaction))
+                        {
+                            command.Parameters.AddWithValue("@id", int.Parse(textBoxID.Text));
+                            command.Parameters.AddWithValue("@nome", textBoxNome.Text);
+                            command.Parameters.AddWithValue("@limite", decimal.Parse(textBoxLimite.Text));
+                            command.ExecuteNonQuery();
+                        }
+
+                        transaction.Commit();
+                    }
+                }
+
+                MessageBox.Show("Registro salvo com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                ClearFields();
+                LoadDataGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao salvar registro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ButtonUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxID.Text))
+            {
+                MessageBox.Show("Selecione um registro para atualizar!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!decimal.TryParse(textBoxLimite.Text, out _))
+            {
+                MessageBox.Show("Insira um valor numérico válido para o limite de crédito!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (var transaction = connection.BeginTransaction())
+                    {
+                        try
+                        {
+                            // Bloquear o registro para evitar alterações simultâneas
+                            var lockQuery = "SELECT nome_cliente, limite_credito FROM clientes WHERE idcliente = @id FOR UPDATE NOWAIT";
+                            using (var lockCommand = new NpgsqlCommand(lockQuery, connection, transaction))
+                            {
+                                lockCommand.Parameters.AddWithValue("@id", int.Parse(textBoxID.Text));
+                                using (var reader = lockCommand.ExecuteReader())
+                                {
+                                    if (reader.Read())
+                                    {
+                                        var nomeDb = reader.GetString(0);
+                                        var limiteDb = reader.GetDecimal(1);
+
+                                        if (nomeDb != currentNome || limiteDb != currentLimite)
+                                        {
+                                            MessageBox.Show("Os dados foram alterados por outro usuário. Operação cancelada.", "Conflito de Dados", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                            transaction.Rollback();
+                                            return;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Registro não encontrado.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                        transaction.Rollback();
+                                        return;
+                                    }
+                                }
+                            }
+
+                            // Confirmar update
+                            var confirmResult = MessageBox.Show("Deseja realmente atualizar este registro?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                            if (confirmResult == DialogResult.No)
+                            {
+                                transaction.Rollback();
+                                return;
+                            }
+
+                            var query = "UPDATE clientes SET nome_cliente = @nome, limite_credito = @limite WHERE idcliente = @id";
+                            using (var command = new NpgsqlCommand(query, connection, transaction))
+                            {
+                                command.Parameters.AddWithValue("@id", int.Parse(textBoxID.Text));
+                                command.Parameters.AddWithValue("@nome", textBoxNome.Text);
+                                command.Parameters.AddWithValue("@limite", decimal.Parse(textBoxLimite.Text));
+                                command.ExecuteNonQuery();
+                            }
+
+                            transaction.Commit();
+                            MessageBox.Show("Registro atualizado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            ClearFields();
+                            LoadDataGrid();
+                        }
+                        catch (PostgresException ex) when (ex.SqlState == "55P03") // 55P03 = Lock not available
+                        {
+                            MessageBox.Show("Este registro está sendo alterado por outro usuário. Tente novamente mais tarde.", "Conflito de Dados", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Erro ao atualizar registro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            transaction.Rollback();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao atualizar registro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Deseja realmente excluir este registro?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirmResult == DialogResult.Yes)
+            {
+                DeleteRecord();
+            }
+        }
+
+        private void DeleteRecord()
+        {
+            if (string.IsNullOrWhiteSpace(textBoxID.Text))
+            {
+                MessageBox.Show("Selecione um registro para excluir!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (var transaction = connection.BeginTransaction())
+                    {
+                        // Bloquear o registro para evitar alterações simultâneas
+                        var lockQuery = "SELECT * FROM clientes WHERE idcliente = @id FOR UPDATE";
+                        using (var lockCommand = new NpgsqlCommand(lockQuery, connection, transaction))
+                        {
+                            lockCommand.Parameters.AddWithValue("@id", int.Parse(textBoxID.Text));
+                            lockCommand.ExecuteNonQuery();
+                        }
+
+                        var query = "DELETE FROM clientes WHERE idcliente = @id";
+                        using (var command = new NpgsqlCommand(query, connection, transaction))
+                        {
+                            command.Parameters.AddWithValue("@id", int.Parse(textBoxID.Text));
+                            command.ExecuteNonQuery();
+                        }
+
+                        transaction.Commit();
+                    }
+                }
+
+                MessageBox.Show("Registro excluído com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                ClearFields();
+                LoadDataGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao excluir registro: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var row = dataGridView1.Rows[e.RowIndex];
+
+                // Verificar se o registro é novo
+                if (row.Cells["idcliente"].Value == null || string.IsNullOrWhiteSpace(row.Cells["idcliente"].Value.ToString()))
+                {
+                    ClearFields();
+                    buttonSave.Enabled = false;
+                    buttonUpdate.Enabled = false;
+                    buttonDelete.Enabled = false;
+                    return;
+                }
+
+                try
+                {
+                    textBoxID.Text = row.Cells["idcliente"].Value.ToString();
+                    textBoxNome.Text = row.Cells["nome_cliente"].Value.ToString();
+
+                    using (var connection = new NpgsqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        var query = "SELECT limite_credito FROM clientes WHERE idcliente = @id";
+                        using (var command = new NpgsqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@id", int.Parse(textBoxID.Text));
+                            textBoxLimite.Text = command.ExecuteScalar()?.ToString();
+                        }
+                    }
+
+                    currentNome = textBoxNome.Text;
+                    currentLimite = decimal.Parse(textBoxLimite.Text);
+
+                    buttonSave.Enabled = false;
+                    buttonUpdate.Enabled = true;
+                    buttonDelete.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao processar o registro selecionado: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void ValidateInputs(object sender, EventArgs e)
+        {
+            buttonSave.Enabled = !string.IsNullOrWhiteSpace(textBoxID.Text) &&
+                                 !string.IsNullOrWhiteSpace(textBoxNome.Text) &&
+                                 !string.IsNullOrWhiteSpace(textBoxLimite.Text);
+        }
+
+        private void RestrictToNumbers(KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void RestrictToLetters(KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar) && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void RestrictToDecimal(TextBox textBox, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+
+            // Permitir apenas uma vírgula
+            if (e.KeyChar == ',' && textBox.Text.Contains(","))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void ClearFields()
+        {
+            textBoxID.Clear();
+            textBoxNome.Clear();
+            textBoxLimite.Clear();
+
+            buttonSave.Enabled = false;
+            buttonUpdate.Enabled = false;
+            buttonDelete.Enabled = false;
+        }
+
+        private void LoadDataGrid()
+        {
+            try
+            {
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    var query = "SELECT idcliente, nome_cliente, limite_credito FROM clientes";
+                    var adapter = new NpgsqlDataAdapter(query, connection);
+                    var table = new DataTable();
+                    adapter.Fill(table);
+
+                    dataGridView1.DataSource = table;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao carregar dados: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
